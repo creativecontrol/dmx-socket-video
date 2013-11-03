97d0738160e1354ca0c9d86efa6ade330718b62c
Thadeus Reed <tcfr33@Ubuntu-Laptop.(none)>
2013-11-03 10:52:09 -0800

    Fixed startup script to deal with pointer and Chrome restore

--- startup.sh
+++ startup.sh
@@ -9,6 +9,9 @@ function nodeStart(){
 }
 
 function pyStart(){
+	sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences 
+	sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chrome/Default/Preferences 
+
 	/usr/bin/python dmxSock.py	
 }
 
@@ -17,3 +20,5 @@ cd ~/Show
 nodeStart &
 sleep 10
 pyStart &
+sleep 10
+xdotool mousemove 50 50
